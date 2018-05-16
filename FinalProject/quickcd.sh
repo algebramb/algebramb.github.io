570110e2fea424358df260a6baa46f83dd634128
Alfonzo Hernandez <ahernandez1133@it515562.ad.ppsnet>
2018-05-16 09:36:13 -0700

    new changes

---
+++ FinalProject/quickcd.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#set this varible on line 3 replace the initials and username
+myDir=Student/J/R/jrowe/ddccode2
+
+
+echo -e .
+echo -e  if you forget to connect using your folder alias, this fails 
+echo -e .
+
+
+
+
+read -p "Press [Enter] key to continue"
+# pause and wait for user to hit enter on keyboard
+
+
+
+echo
+
+# cp is copy command
+
+cp .  /Volumes/Student/A/H/AHernandez1133/ddccode1/assignment4/finalproject
+
+
+
+echo
